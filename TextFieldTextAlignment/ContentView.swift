5b868ae55d6617230fbb31e6725c6a981a378c75
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-17 09:19:12 +0300

    init

--- TextFieldTextAlignment/ContentView.swift
+++ TextFieldTextAlignment/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textFieldData1 = "Leading"
+    @State private var textFieldData2 = "Center"
+    @State private var textFieldData3 = "Trailing"
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 20) {
+            Text("TextField").font(.largeTitle)
+            Text("Text Aligment").font(.title).foregroundColor(.gray)
+            Text("Change the aligment of text within your textfield by using the multilineTextAligment modifier.")
+                .frame(maxWidth: .infinity).padding()
+                .background(Color.orange)
+                .font(.title)
+            
+            Group {
+                TextField("Leading", text: $textFieldData1)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.leading) // Default
+                
+                TextField("Center", text: $textFieldData2)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.center)
+                
+                TextField("Trailing", text: $textFieldData3)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .multilineTextAlignment(.trailing)
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
